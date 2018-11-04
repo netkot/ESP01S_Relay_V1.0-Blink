d334d639e7658fe5ce018c3d77533bc2d4c3fd3f
netkot <netkot@gmail.com>
2018-11-04 22:27:21 +0200

    Add files via upload

---
+++ ESP-01S_Relay_V1.0_Blink_Test.ino
@@ -0,0 +1,36 @@
+/*  
+    ESP-01S Relay V1.0 Blink Test
+    
+    China module fix:
+    Solder VCC to CH_EN
+    Solder VCC over 10k resistor to GPIO 0
+
+
+           o o
+           o o--\ CHIP_EN
+ GPIO 0 /--o o  |
+        |  o o--| VCC
+        |       |
+        \-[10k]-/
+
+ 
+*/
+
+int i = 1;
+int delay_sec = 1000;
+
+
+void setup() {
+  pinMode(i, OUTPUT);
+  pinMode(0, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite (i, HIGH);
+  digitalWrite (0, HIGH);
+  delay (delay_sec);
+  digitalWrite (i, LOW);
+  digitalWrite (0, LOW);
+  delay (delay_sec);
+}
